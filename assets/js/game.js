99f84eb6774d4f908d006faadaba6a86740cdf61
AndreBlankholm <ablankholm@gmail.com>
2022-09-19 22:28:01 -0500

    constructing user inputs

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var playerName = window.prompt("What is your players name?");
+
+window.alert( "Your players name is "+ playerName);
+window.alert("Welcome to the fight of your life " + playerName + "!");
+console.log(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+
+
+
+
+//fight();
