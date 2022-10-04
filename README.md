6c679dd7039dbda0a9d85a4218a41e608054891d
AndreBlankholm <ablankholm@gmail.com>
2022-10-04 15:00:53 -0500

    Completed README file

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Blow-4-Blow
-Using JavaScript to build a browser-based attack simulator game.
+
+
+## Using JavaScript to build a browser-based attack simulator game.
+
+
+#
+
+## I wanted a good refresher app that I used to: 
+ * communicate with the game player by using JavaScript functions.
+ * Assign and manipulate data using JavaScript variables and operators.
+ * Control the flow of the application by managing conditional statements.
+
+## Links to project: 
+### GitHub:  https://github.com/AndreBlankholm/Blow-4-Blow
+### Live URL: https://andreblankholm.github.io/Blow-4-Blow/
+
+
+
+## License & copyright & Made with ❤️ by Andre Blankholm
--- assets/js/game.js
+++ assets/js/game.js
@@ -4,85 +4,102 @@
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
-var playerName = window.prompt("What is your players name?");
-var playerAttack = 20;
-var playerHealth = 100;
-var playerMoney = 10;
+// just adding the randomization of numbers function anywhere in this file. perferrably at the bottom
+var randomNumberGen = function() {
+  var value = Math.floor(Math.random() * 21) + 40;
+  return value;
+  };
+  
 
-window.alert( "Your players name is "+ playerName);
+var playerInfo = {
+  name: window.prompt(" Welcom to BLOW-4_Blow!   What is your player's name?"),
+  health: 100,
+  attack: 10,
+  money:20
+};
+
+window.alert( "Your players name is "+ playerInfo.name);
 
 
-var enemyNames = ["Big Mook", "LIL Sam", "The Big Boss"];
-var enemyAttack = 12;
-var enemyHealth = 50;
+var enemyInfo = [
+  {
+    name: 'Big Mook',
+    attack: randomNumberGen(10, 14)
+  },
+  {
+    name: 'lil Sam',
+    attack: randomNumberGen(10, 14)
+  },
+  {
+    name: 'Big Boss The Great',
+    attack: randomNumberGen(10, 14)
+  }
+];
 
 //console.log(enemyNames,enemyAttack,enemyHealth);
 
 
 
-var fight = function(enemyName) {
-   // both enemy and player must both have health inorder to fight
-  while(enemyHealth > 0 && enemyHealth > 0) {
-    var promptFight = window.prompt("Would you lik to Fight or Skip this battle?  Enter 'Fight' or 'Skip' to choose.");
+var fight = function(enemy) {
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    // ask player if they'd like to fight or run
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
-      var confirmSkip = window.confirm("Are you sure you want to skip?");
+      // confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
+      // if yes (true), leave fight
       if (confirmSkip) {
-        window.alert("Player has chosen to skip this round.");
-        window.alert(playerName + " has chosen to skip the fight.  Goodbye!");
-        playerMoney = Math.max(0, playerMoney - 10);
-        console.log(playerHealth,playerMoney);
-        window.alert(playerName + " has " + playerMoney + " Worth of money.");
+        window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
+        // subtract money from playerInfo.money for skipping
+        playerInfo.money = Math.max(0, playerInfo.money - 10);
+        console.log("playerInfo.money", playerInfo.money)
         break;
-      } 
-    } 
-    // place fight function code block here . . .
-    
-    if (promptFight === "fight" || promptFight === "FIGHT" || promptFight === "f" || promptFight === "F") {
-
-      // generate random damage value based on player's attack power
-      var damage = randomNumberGen(playerAttack - 3, playerAttack);
-
-      enemyHealth = Math.max(0, enemyHealth - damage);
-
-      console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-      );
-    
-      // check enemy's health
-      if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
-        break;
-      } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-        playerMoney = playerMoney + 20;
       }
-    
-      // remove player's health by subtracting the amount set in the enemyAttack variable
-
-      var damage = randomNumberGen(enemyAttack - 3, enemyAttack);
-
-      playerHealth = Math.max(0, playerHealth - damage);
-
-      console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-      );
-    
-      // check player's health
-      if (playerHealth <= 0) {
-        window.alert(playerName + " has died!");
-        break;
-
-      } else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
-      }
-      // if player choses to skip
-    } else {
-      window.alert("You need to choose a valid option. Try again!");
-      
     }
-  }
-};
+
+    // generate random damage value based on player's attack power
+    var damage = randomNumberGen(playerInfo.attack - 3, playerInfo.attack);
+
+    enemy.health = Math.max(0, enemy.health - damage);
+    console.log(
+      playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
+    );
+
+    // check enemy's health
+    if (enemy.health <= 0) {
+      window.alert(enemy.name + ' has died!');
+
+      // award player money for winning
+      playerInfo.money = playerInfo.money + 20;
+
+      // leave while() loop since enemy is dead
+      break;
+    } else {
+      window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
+    }
+
+    // remove players's health by subtracting the amount set in the enemy.attack variable
+    var damage = randomNumberGen(enemy.attack - 3, enemy.attack);
+
+    playerInfo.health = Math.max(0, playerInfo.health - damage);
+    
+    console.log(
+      enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health + ' health remaining.'
+    );
+
+    // check player's health
+    if (playerInfo.health <= 0) {
+      window.alert(playerInfo.name + ' has died!');
+      // leave while() loop if player is dead
+      break;
+    } else {
+      window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
+    }
+  } // end of while loop
+}; // end of fight function
 
 
 // this for loop lets me access every index in the array of (enemyNames)
@@ -90,28 +107,28 @@ var fight = function(enemyName) {
 
 var startGame = function() {
 
-  //at the start of the game I need to reset the theplayer stats
-  playerHealth = 100;
-  playerAttack = 10;
-  playerMoney = 10;
+  playerInfo.health = 100;
+  playerInfo.attack = 10;
+  playerInfo.money = 10;
 
-  for(var i = 0; i < enemyNames.length; i++) {
+  for(var i = 0; i < enemyInfo.length; i++) {
 
-    if (playerHealth > 0) {
+    if (playerInfo.health > 0) {
        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert("Welcome to Blow-4-Blow! Round " + (i + 1));
   
       // pick new enemy to fight based on the index of the enemyNames array
-      var pickedEnemyName = enemyNames[i];
-      window.alert(" Your fighting " + enemyNames[i] + " and your have " + playerMoney + " worth of money so fight or skip like your life depended on it " + playerName + " !");
+      var pickedEnemyObj = enemyInfo[i];
+      
+      
       // reset enemyHealth before starting new fight
-      enemyHealth = randomNumberGen(40, 60);
-  
+      pickedEnemyObj.health = randomNumberGen(40, 60);
+      
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-      fight(pickedEnemyName);
-
+      fight(pickedEnemyObj);
+      
       //enter shop if there is still room on enemy array
-      if (playerHealth > 0 && i < enemyNames.length - 1 && playerMoney >= 7) {
+      if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
 
         var storeConfirm = window.confirm(" Do you want to enter the shop before the next round to upgrade?");
         
@@ -130,8 +147,8 @@ var startGame = function() {
 
 var endGame = function() {
 
-  if (playerHealth > 0) {
-    window.alert("Great job, you survived the game! You know have a score of " + playerHealth + " .");
+  if (playerInfo.health > 0) {
+    window.alert("Great job, you survived the game! You know have a score of " + playerInfo.health + " .");
 
   } else {
     window.alert("The game has ended. Let's see how you did!");
@@ -143,7 +160,7 @@ var endGame = function() {
     //restart game
     startGame();
   } else {
-    window.alert(" Thank you " + playerName + " for playing the best windows pop up game ever made, Blow-4-Blow! Please come back and play again anytime.");
+    window.alert(" Thank you " + playerInfo.name + " for playing the best windows pop up game ever made, Blow-4-Blow! Please come back and play again anytime.");
   }
 };
 
@@ -157,12 +174,12 @@ var shop = function() {
     case "HEALTH":
     case "h":
     case "H":
-      if (playerMoney >= 7) {
+      if (playerInfo.money >= 7) {
         window.alert("Refilling player's health by 20 for 7 dollars.");
   
         // increase health and decrease money
-        playerHealth = playerHealth + 20;
-        playerMoney = playerMoney - 7;
+        playerInfo.health = playerInfo.health + 20;
+        playerInfo.money = playerInfo.money - 7;
 
       } else {
         window.alert("You dont have enough money, sorry!")
@@ -174,12 +191,12 @@ var shop = function() {
     case "ATTACK":
     case "a":
     case "A":
-      if (playerMoney >= 7) {
+      if (playerInfo.money >= 7) {
         window.alert("Upgrading player's attack by 6 for 7 dollars.");
 
         
-        playerAttack = playerAttack + 6;
-        playerMoney = playerMoney - 7;
+        playerInfo.attack = playerInfo.attack + 6;
+        playerInfo.money = playerInfo.money - 7;
       }
       else {
         window.alert("You don't have enough money!");
@@ -205,10 +222,5 @@ var shop = function() {
   }
 };
 
-// just adding the randomization of numbers function anywhere in this file. perferrably at the bottom
-var randomNumberGen = function() {
-var value = Math.floor(Math.random() * 21) + 40;
-return value;
-};
 
 startGame();
